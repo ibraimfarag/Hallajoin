e74f6ab9cb2e73ca06f99807eb8e4ace427e1d45
ibraimfarag <ib.farag@gmail.com>
2024-07-01 21:13:47 +0300

    upload

--- themes/BC/Tour/Views/frontend/blocks/testimonial/index.blade.php
+++ themes/BC/Tour/Views/frontend/blocks/testimonial/index.blade.php
@@ -1,62 +1,79 @@
 @if (!empty($reviews))
+
+
+
     <div id="testimonial-carousel" class="bravo-testimonial carousel slide" data-ride="carousel" data-interval="3000">
         <div class="container">
             <h3>{{ $title }}</h3>
+            {{-- <span>{{$chunks }}</span> --}}
             <div class="carousel-inner">
-                @foreach ($reviews as $index => $review)
+
+                @php
+                $chunks = [];
+                if ($_SERVER['HTTP_USER_AGENT']) {
+                    if (preg_match('/(iPhone|iPod|iPad|Android|BlackBerry|BB|PlayBook|Windows Phone|webOS|MeeGo|Palm|Symbian|Bada|BREW|Zune)/', $_SERVER['HTTP_USER_AGENT'])) {
+                        $chunks = array_chunk($reviews, 1);
+                    } else {
+                        $chunks = array_chunk($reviews, 3);
+                    }
+                }
+            @endphp
+                @foreach ($chunks as $index => $reviewGroup)
                     <div class="carousel-item{{ $index == 0 ? ' active' : '' }}">
                         <div class="row justify-content-center">
-                            <div class="col-md-4 col-sm-12">
-                                <div class="testimonial-container">
-                                    <?php
-                                    $avatar_url = get_file_url($review['reviewer']['avatar_id'], 'full') ?? 'https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg';
-                                    $tour_image_url = get_file_url($review['tour']['banner_image_id'], 'full') ?? 'https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg';
-                                    $tour_link = '/tour/' . $review['tour']['slug'];
-                                    ?>
-                                    <div class="testimonial-header" style="background-image: url('{{ $tour_image_url }}'); position: relative; padding: 20px;">
-                                        <h6>{{ $review['tour']['title'] }}</h6>
-                                        @if (!empty($review['rate_number']) && $review['rate_number'] >= 1)
-                                            <div class="rate-number">
-                                                <span> <i class="fa fa-star orange"></i>
-                                                    {{ $review['rate_number'] }}
-                                                    ({{ $review['tour']['reviews_count'] }})
-                                                </span>
-                                            </div>
-                                        @endif
-                                        <a href="{{ $tour_link }}" class="tour-link">
-                                            <i class="fa fa-arrow-right"></i>
-                                        </a>
-                                    </div>
-                                    <div class="item has-matchHeight text-center justify-content-center">
-                                        <div class="authoor justify-content-center">
-                                            <div class="author justify-content-center">
-                                                <div class="col">
-                                                    <div class="row justify-content-center">
-                                                        <img src="{{ $avatar_url }}" alt="{{ $review['reviewer']['name'] }}" class="rounded-circle">
-                                                    </div>
-                                                    <div class="row justify-content-center">
-                                                        <h4>{{ $review['reviewer']['name'] }}</h4>
-                                                    </div>
-                                                    <div class="row justify-content-center">
-                                                        <p>{{ $review['date'] }}</p>
-                                                    </div>
-                                                    <div class="row justify-content-center">
-                                                        @if (!empty($review['rate_number']) && $review['rate_number'] >= 1)
-                                                            <div class="star">
-                                                                @for ($i = 0; $i < $review['rate_number']; $i++)
-                                                                    <i class="fa fa-star orange"></i>
-                                                                @endfor
-                                                            </div>
-                                                        @endif
+                            @foreach ($reviewGroup as $review)
+                                <?php
+                                $avatar_url = get_file_url($review['reviewer']['avatar_id'], 'full') ?? 'https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg';
+                                $tour_image_url = get_file_url($review['tour']['banner_image_id'], 'full') ?? 'https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg';
+                                $tour_link = '/tour/' . $review['tour']['slug'];
+                                ?>
+                                <div class="col-md-4 col-sm-12">
+                                    <div class="testimonial-container">
+                                        <div class="testimonial-header" style="background-image: url('{{ $tour_image_url }}'); position: relative; padding: 20px;">
+                                            <h6>{{ $review['tour']['title'] }}</h6>
+                                            @if (!empty($review['rate_number']) && $review['rate_number'] >= 1)
+                                                <div class="rate-number">
+                                                    <span> <i class="fa fa-star star-red"></i>
+                                                        {{ $review['rate_number'] }}
+                                                        ({{ $review['tour']['reviews_count'] }})
+                                                    </span>
+                                                </div>
+                                            @endif
+                                            <a href="{{ $tour_link }}" class="tour-link">
+                                                <i class="fa fa-arrow-right"></i>
+                                            </a>
+                                        </div>
+                                        <div class="item has-matchHeight text-center justify-content-center">
+                                            <div class="authoor justify-content-center">
+                                                <div class="author justify-content-center">
+                                                    <div class="col">
+                                                        <div class="row justify-content-center">
+                                                            <img src="{{ $avatar_url }}" alt="{{ $review['reviewer']['name'] }}" class="rounded-circle">
+                                                        </div>
+                                                        <div class="row justify-content-center">
+                                                            <h4>{{ $review['reviewer']['name'] }}</h4>
+                                                        </div>
+                                                        <div class="row justify-content-center">
+                                                            <p>{{ $review['date'] }}</p>
+                                                        </div>
+                                                        <div class="row justify-content-center">
+                                                            @if (!empty($review['rate_number']) && $review['rate_number'] >= 1)
+                                                                <div class="star">
+                                                                    @for ($i = 0; $i < $review['rate_number']; $i++)
+                                                                        <i class="fa fa-star star-red"></i>
+                                                                    @endfor
+                                                                </div>
+                                                            @endif
+                                                        </div>
                                                     </div>
                                                 </div>
+                                                <i class="fa fa-quote-left" style="font-size: 25px; color: #8080807d;"></i>
+                                                <p>{{ $review['content'] }}</p>
                                             </div>
-                                            <i class="fa fa-quote-left" style="font-size: 25px; color: #8080807d;"></i>
-                                            <p>{{ $review['content'] }}</p>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 @endforeach
@@ -69,12 +86,12 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
-            <ol class="carousel-indicators">
-                @foreach ($reviews as $index => $indicator)
-                    <li data-target="#testimonial-carousel" data-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
-                @endforeach
-            </ol>
         </div>
+        <ol class="carousel-indicators">
+            @foreach (range(0, count($chunks) - 1)  as $index => $indicator)
+                <li data-target="#testimonial-carousel" data-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
+            @endforeach
+        </ol>
     </div>
 
     <style>
@@ -95,7 +112,6 @@
         .testimonial-header {
             position: relative;
             height: 200px;
-            /* Adjust as needed */
             background-size: cover;
             background-position: center;
             border-top-left-radius: 8px;
@@ -138,23 +154,17 @@
             z-index: 99999;
         }
 
-        @media (min-width: 768px) {
-            .carousel-inner .carousel-item {
-                /* display: flex; */
-                flex-wrap: wrap;
-                justify-content: center;
-            }
-            .carousel-inner .carousel-item .col-md-4 {
-                display: block;
+        @media (max-width: 767.98px) {
+            .col-sm-12 {
+                flex: 0 0 100%;
+                max-width: 100%;
             }
         }
 
-        @media (max-width: 767.98px) {
-            .carousel-inner .carousel-item .col-md-4 {
-                display: none;
-            }
-            .carousel-inner .carousel-item .col-sm-12 {
-                display: block;
+        @media (min-width: 768px) {
+            .col-md-4 {
+                flex: 0 0 33.333333%;
+                max-width: 33.333333%;
             }
         }
     </style>
